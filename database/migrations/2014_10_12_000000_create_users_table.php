10ec13ba073b39acf8aa6b7420b8d5453ea64f17
_WEBNIK <elit15223@ssu.edu.ua>
2017-11-23 01:34:11 +0300

    feature(migrations)

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_user_id')->unsigned();
+            $table->string('name');
+            $table->string('email',100)->unique();
+            $table->boolean('email_verify');
+            $table->string('email_token');
+            $table->string('password');
+            $table->boolean('online');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2017_11_22_203903_create_type_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+             $table->foreign('type_user_id')->references('id')->on('type_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+             $table->dropForeign(['type_user_id']);
+        });
+        Schema::dropIfExists('type_users');
+    }
+}
