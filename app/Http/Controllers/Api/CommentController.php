a2241d52eb14b6108a677ac5a5254db4fb34d804
_WEBNIK <elit15223@ssu.edu.ua>
2018-02-20 16:15:42 +0200

    CommentController(not tested)

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Comment;
 
 class CommentController extends Controller
 {
+    public function getAllComments() {
+      
+            $data = [
+                'status' => 1,
+                'data' => Comment::latest()->get(),
+            ];
+
+        return response()->json($data);
+    }
     public function getPostsComments() {
     	$validator = Validator::make($request->all(),[
     		'post_id' => 'requred|exists:posts,id'
@@ -28,7 +38,7 @@ class CommentController extends Controller
     }
 
     public function getUsersComments() {
-    	$validator = Validator::($request->all(),[
+    	$validator = Validator::make($request->all(),[
     		'user_id' => 'required|exists:users,id',
     	]);
 
@@ -49,7 +59,7 @@ class CommentController extends Controller
     }
 
     public function getComment(Request $request){
-    	$validator = Validator::($request->all(),[
+    	$validator = Validator::make($request->all(),[
     		'comment_id' => 'required|exists:comments,id',
     	]);
 
@@ -67,5 +77,77 @@ class CommentController extends Controller
     	}
 
     	return response()->json($data);
-    } 
+    }
+
+    public function putComment(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'comment_id' => 'required|exists:comments,id',
+            'comment' => 'string|min:1',
+        ]);
+
+        if($validator->fails()) {
+           $data = [
+                'status' => 0,
+                'errors' => $validator->errors(),
+           ]; 
+        } else {
+            $comment = Comment::find($request->input('comment_id'));
+
+            if(!empty($request->input('comment'))) {
+               $comment->comment = $request->input('comment');
+            }
+             
+             $data = [
+                'status' => 1,
+             ];
+        }
+
+        return response()->json($data);
+    }
+
+    public function setComment(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'user_id' => 'required|exists:users,id',
+            'comment' => 'required|string|min:1',
+            'post_id' => 'required|exists:posts,id',
+        ]);
+
+        if($validator->fails()) {
+            $data = [
+                'status' => 0,
+                'errors' => $validator->errors(),
+            ];
+        } else {
+            $comment = new Comment();
+            $comment->user_id = $request->input('user_id');
+            $comment->post_id = $request->input('post_id'); 
+            $comment->comment = $request->input('comment'); 
+            $comment->read = false;
+
+            $data = [
+                'status' => 1,
+            ];  
+        }
+
+        return response()->json($data);
+    }
+
+    public function deleteComment(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'comment_id' => 'required|exists:comments,id'
+        ]);
+        if($validator->fails()) {
+            $data = [
+                'status' => 0,
+                'errors' => $validator->errors(),
+            ];
+        } else {
+            Comment::destroy($request->input('comment_id'));
+            $data = [
+                'status' => 1,
+            ];
+        }
+
+        return response()->json($data);
+    }
 }
